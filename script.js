30936615f11e07d699d3918d6e1d28f104ab83b8
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-12-09 17:55:42 -0700

    add fetch features

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var searchBtn = document.querySelector('.searchBtn')
-const url = `https://api.openweathermap.org/data/2.5/weather?lat={39.742043}&lon={-104.991531}&appid=202f47e2a9f5284ca038475e5bd1cccd}`
+var denverBtn = document.querySelector('.denverBtn')
+var cityTag = document.querySelector('.cityTag')
+var searchBtn = document.querySelector('.searchBtn')
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=39.742043&lon=-104.991531&appid=0f767ba75fcf16c87cbd0741a061e947`;
 
-searchBtn.addEventListener('click', citySearch)
+// searchBtn.addEventListener('click', citySearch)
+denverBtn.addEventListener('click', cityDenver)
 
-function citySearch() {
+
+function cityDenver() {
     fetch(url)
     .then((response) => response.json())
     .then((data) => console.log(data)
-    )}
+    )
+}
+cityTag.innerHTML = `CITY: ${data.name}`;
