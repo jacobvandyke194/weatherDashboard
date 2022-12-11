67e31e20eff06acd4d628f39819747a02e1754ce
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-12-11 13:53:30 -0700

    finished functionality of search

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 var searchBtn = document.querySelector('.searchBtn')
 var denverBtn = document.querySelector('.denverBtn')
 var cityTag = document.querySelector('.cityTag')
+var windTag = document.querySelector('.windTag')
+var tempTag = document.querySelector('.tempTag')
+var humidityTag = document.querySelector('.humidityTag')
 var searchBtn = document.querySelector('.searchBtn')
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=39.742043&lon=-104.991531&appid=0f767ba75fcf16c87cbd0741a061e947`;
+var searchInput = document.querySelector('.searchInput')
+var latitude;
+var longitude;
 
-// searchBtn.addEventListener('click', citySearch)
-denverBtn.addEventListener('click', cityDenver)
+searchBtn.addEventListener('click', citySearch)
 
 
-function cityDenver() {
-    fetch(url)
+function citySearch() {
+    var city = searchInput.value;
+    const coordinateApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0f767ba75fcf16c87cbd0741a061e947`
+    fetch(coordinateApi)
     .then((response) => response.json())
-    .then((data) => console.log(data)
-    )
-}
-cityTag.innerHTML = `CITY: ${data.name}`;
+    .then(function (data) {
+        console.log(data);
+        latitude = data.coord.lat;
+        longitude = data.coord.lon;
+        
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0f767ba75fcf16c87cbd0741a061e947`;
+        fetch(url)
+        .then((response) => response.json())
+        .then(function (data) {
+        cityTag.innerHTML = `CITY: ${data.name}`;
+        tempTag.innerHTML = `TEMP: ${data.main.temp} DEGREES`;
+        windTag.innerHTML = `WIND: ${data.wind.speed} MPH`;
+        humidityTag.innerHTML = `HUMIDITY: ${data.main.humidity}%`;
+
+    })
+    }
+    )}
+
+    
+
+
+
